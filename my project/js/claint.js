a533ad64cc091862b770ec5d4b423c19ddaa09fc
talia ben hamo <106983102+Taliabenhamo@users.noreply.github.com>
2022-09-21 17:16:09 +0300

    Add files via upload

---
+++ my project/js/claint.js
@@ -0,0 +1,51 @@
+export let myArr=[
+{title:"walk on's",
+info:"Despite having little business experience and even less financial backing, Brandon and Jack incorporated the traits they learned on the basketball court  hard work, dedication and a commitment to excellence - into their restaurant vision. The two were determined to create a business environment built on culture and the idea that everyone on the team was needed and mattered as much as the next guy. On September 9, 2003, their shared dream became a reality as Walk-Ons Bistreaux & Bar® opened for business in the shadows of LSU’s Tiger Stadium.",
+img:"https://wpcdn.us-midwest-1.vip.tn-cloud.net/www.bizneworleans.com/content/uploads/data-import/b1e09805/Covington-To-Welcome-1st-Walk-Ons-Restaurant-1024x683.jpg",
+contact:""
+
+},
+{title:"Sheraton New Orleans",
+info:"Everything you need for a relaxing vacation, a successful meeting, a memorable event...it's all waiting for you at the Sheraton New Orleans Hotel. Just steps from the French Quarter, the Sheraton New Orleans Hotel provides easy access to world-class dining, live music, great shopping and more. Our 1,110 non-smoking guest rooms, including 53 suites, reflect New Orleans charm and many have views of the Mississippi River or French Quarter. The warm décor is anchored by the supremely comfortable Sheraton Sweet Sleeper™ Bed and accented in inviting hues. Sample regional cuisine at Roux Bistro or enjoy a cocktail at the enchanting Pelican Bar. Then stop by our state-of-the-art fitness center or outdoor pool. We also offer a business center and 105,700 sq ft, a total of 53 rooms, of flexible meeting space.",
+img:"https://images.getaroom-cdn.com/image/upload/s--ls7oq1VW--/c_limit,e_improve,fl_lossy.immutable_cache,h_940,q_auto:good,w_940/v1526342547/77a9d14fa7bb2116a83245ddb43b869532d04895?atc=e7cd1cfa",
+contact:"(03)7219458",
+
+},
+{title:"",
+info:"",
+img:"",
+contact:"",
+
+
+},
+{title:"The Ritz carlaton",
+info:"Property Location With a stay at The Ritz-Carlton, New Orleans in New Orleans (French Quarter), you'll be steps from Canal Street and Bourbon Street. This 4.5-star hotel is 0.7 mi (1.1 km) from Audubon Aquarium of the Americas and 0.7 mi (1.1 km) from Jackson Square. Rooms Make yourself at home in one of the 527 air-conditioned rooms featuring iPod docking stations and Smart televisions. Satellite programming and video-game consoles are provided for your entertainment, with wired and wireless Internet access available for a surcharge. Private bathrooms with shower/tub combinations feature designer toiletries and hair dryers. Conveniences include phones, as well as laptop-compatible safes and desks. Amenities Relax at the full-service spa, where you can enjoy massages, body treatments, and facials. You're sure to appreciate the recreational amenities, including an indoor pool, a sauna, and a fitness center. Additional amenities at this hotel include complimentary wireless Internet access, concierge services, and gift shops/newsstands. Dining Grab a bite to eat at M Bistro, a restaurant which features a bar, or stay in and take advantage of the 24-hour room service. Cooked-to-order breakfasts are available daily from 6:30 AM to 11:00 AM for a fee. Business, Other Amenities Featured amenities include complimentary wired Internet access, a 24-hour business center, and express check-in. This hotel has 15 meeting rooms available for events.",
+img:"https://images.getaroom-cdn.com/image/upload/s--dClOfWgt--/c_limit,e_improve,fl_lossy.immutable_cache,h_940,q_auto:good,w_940/v1603449961/2a6f1e05da3134f1e4799ebb8472145214f99529?atc=4e8c2e94",
+contact:"+1 (504) 524-1331",
+
+
+},
+{title:"",
+info:"",
+img:"",
+contact:"",
+
+
+},
+{title:"louisiana children's museum",
+info:"When you make a child’s potential visible, wonderful things happen. So we created a place where kids use play, shared exploration, and conversation to connect with the people and world around them. A place where kids can be the amazing humans they are. A place where wonderful things happen daily.",
+img:"https://1.bp.blogspot.com/-xMisyRvEy7M/Xbsnd4ENAtI/AAAAAAAADcI/3j2npCZbadwdsIPjO2Ahz23ZxV1cbGToQCLcBGAsYHQ/s1600/2LCM_1200_570_c1.jpeg",
+contact:"+1 504-523-1357",
+
+
+},
+{title:"",
+info:"",
+img:"",
+contact:"",
+
+
+}
+
+
+]
